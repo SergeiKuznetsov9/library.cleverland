b872b50dba82bce0f29d598662fe29cedcd3f675
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-08-28 13:28:05 +0300

    feat: profile admin page

---
+++ src/pages/admin-profile/admin-profile-page.tsx
@@ -0,0 +1,25 @@
+import { Loader } from '../../components/loader/loader';
+import { useAppSelector } from '../../store/hooks';
+import { getUserSelector } from '../../store/user/selectors';
+
+import { ProfileHeader } from './profile-header';
+import { ProfileBody } from './ptofile-body';
+
+import styles from './admin-profile-page.module.scss';
+
+export const AdminProfilePage = () => {
+    const { data: user, isLoading } = useAppSelector(getUserSelector);
+
+    return (
+        <div className={styles.wrapper} data-test-id='profile-page'>
+            {isLoading && <Loader />}
+            <ProfileHeader
+                avatar={user.avatar}
+                id={user.id}
+                userFirstName={user?.firstName}
+                userLastName={user?.lastName}
+            />
+            <ProfileBody user={user} />
+        </div>
+    );
+};
